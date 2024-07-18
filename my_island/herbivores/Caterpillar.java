c3750b6c35914c7e3d27b16c33530431d668b76c
Ranganathan Kasiganesan <ranganath73@gmail.com>
2024-07-18 16:54:35 +0530

    Animals behaviors added to classes

---
+++ my_island/herbivores/Caterpillar.java
@@ -0,0 +1,19 @@
+// I am dead. I am not existing in this world.
+// I will eat Plant: 100%
+// I will starve to death 100%
+// So, my lifetime is very less than the lifetime of this Game.
+// ### SO I WON'T PARTICIPATE IN THIS GAME ###
+public class Caterpillar {
+    public void eat();
+
+    public void sleep();
+
+    public void walk();
+
+    public void run();
+
+    public void jump();
+
+    public void hunt();
+
+}
---
+++ my_island/herbivores/Duck.java
@@ -0,0 +1,14 @@
+public class Duck implements AnimalBehavior {
+    public void eat();
+
+    public void sleep();
+
+    public void walk();
+
+    public void run();
+
+    public void jump();
+
+    public void hunt();
+
+}
